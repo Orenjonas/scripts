d5b3e2415edf56a1e62313f9d150c5503622612d
Jonas Øren <jonas.oren@gmail.com>
2019-01-31 13:03:38 +0100

    docstring

--- python/punchclock.py
+++ python/punchclock.py
@@ -1,5 +1,17 @@
 import subprocess, sys, os
 
+"""
+A short script for timing taskt.
+printing the time between punching in and punching out.
+
+Usage:
+    >>> python3 punchclock.py start
+    Timer startet
+    >>> python3 punchclock.py slutt
+    tid:  00:00:05
+"""
+
+
 p = subprocess.Popen(['date', '+%s'], stdout=subprocess.PIPE, universal_newlines=True)
 time, err = p.communicate()
 
